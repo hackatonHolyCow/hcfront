10470914aa800fccf837bd2ee3f12b1d695e9093
DarioRoman01 <darioroman2001@gmail.com>
2024-10-05 21:20:07 -0300

    feat: mp

--- src/app/api/audio.js
+++ src/app/api/audio.js
@@ -21,3 +21,26 @@ export const PostUploadAudio = async (audio) => {
     console.log("Error on PostUploadAudio", error);
   }
 };
+
+
+export const PostOrder = async (order) => {
+  try {
+    const response = await fetch("http://localhost:8000/api/v1/orders", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(order),
+    })
+
+    if (!response.ok) {
+      throw new Error("Error on PostOrder");
+    }
+
+    const body = await response.json();
+    console.log("EL CUERPECITO", body);
+    return body;
+  } catch(err) {
+    throw new Error(err);
+  }
+}
--- src/app/components/Menu.js
+++ src/app/components/Menu.js
@@ -1,6 +1,9 @@
+"use client";
+
 import React from "react";
 import { Box, Card, CardContent, CardMedia, Typography } from "@mui/material";
 import Grid from "@mui/material/Grid2";
+import { MPCheckout } from "./MpCard";
 
 const Menu = () => {
   const items = [
